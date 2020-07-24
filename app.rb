d96048479e4704b94296f7cc77fc498701cb4ebd
NedosekinStas <stasnedosekin@mail.ru>
2020-07-24 17:58:51 +0300

    function enter withdrawal word

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 file_name = File.open 'file.txt', 'r'
 
+hh = {}
+
+def add_to_hash word
+  puts word
+end
+
 file_name.each_line do |line|
-  arr = line.split (/ /)
-  puts arr.inspect
+  arr = line.split (/\s|\n|\.|,/) # \s (white_space), | (or), \n (new string), \ (escape)
+  arr.each {|word| add_to_hash(word)}
 end   
 
 f.close
